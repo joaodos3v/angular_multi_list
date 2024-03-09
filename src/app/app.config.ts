acb79dd9988c508547ddc5cf21953fc2e498745f
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-03-09 20:25:54 -0300

    feat: add dynamic providers with page reload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,9 +5,22 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 
 import { provideApplication } from './application/provide-application';
+import { USE_POKE_API_KEY } from './application/services/source.service';
 import { providePokeAPI } from './adapters/secondary/pokeapi/provide-pokeapi';
-// import { provideNarutoAPI } from './adapters/secondary/narutoapi/provide-narutoapi';
+import { provideNarutoAPI } from './adapters/secondary/narutoapi/provide-narutoapi';
+
+const externalProviderFactory = () => {
+  const shouldUsePokeAPI = Boolean(window.localStorage.getItem(USE_POKE_API_KEY));
+  const provider = shouldUsePokeAPI === true ? providePokeAPI : provideNarutoAPI;
+
+  return [...provider()];
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch()), ...provideApplication(), ...providePokeAPI()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    ...provideApplication(),
+    ...externalProviderFactory(),
+  ],
 };
