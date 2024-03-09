6b05f1aa6aad4483f7b2d01b56881756ae9e1a5a
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-03-09 12:21:04 -0300

    feat: fill list cards with external items

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './card.component.html',
 })
-export class CardComponent {}
+export class CardComponent {
+  @Input()
+  title: string = '';
+
+  @Input()
+  description: string = '';
+
+  @Input()
+  imageURL: string = '';
+}
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Item } from '../../domain/models/item.model';
 import { CardComponent } from '../card/card.component';
+import { LIST_PROVIDER } from '../../providers/services/list-service.provider';
 
 @Component({
   selector: 'app-list',
@@ -7,4 +9,16 @@ import { CardComponent } from '../card/card.component';
   imports: [CardComponent],
   templateUrl: './list.component.html',
 })
-export class ListComponent {}
+export class ListComponent implements OnInit {
+  listService = inject(LIST_PROVIDER);
+
+  items: Item[] = [];
+
+  ngOnInit(): void {
+    this.getInitialItems();
+  }
+
+  async getInitialItems() {
+    this.items = await this.listService.getItems();
+  }
+}
