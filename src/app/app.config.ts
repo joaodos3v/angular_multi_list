18712f49d71532be5660e865f68af05189c0dfe1
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-03-09 13:11:00 -0300

    feat(AppConfig): replace NarutoAPI by PokeAPI to test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,9 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 
 import { provideApplication } from './application/provide-application';
-import { provideNarutoAPI } from './adapters/secondary/narutoapi/provide-narutoapi';
+import { providePokeAPI } from './adapters/secondary/pokeapi/provide-pokeapi';
+// import { provideNarutoAPI } from './adapters/secondary/narutoapi/provide-narutoapi';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch()), ...provideApplication(), ...provideNarutoAPI()],
+  providers: [provideRouter(routes), provideHttpClient(withFetch()), ...provideApplication(), ...providePokeAPI()],
 };
