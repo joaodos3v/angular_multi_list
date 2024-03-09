41a003f511f35609bc0c1c2bf7e5ffd8b7c91815
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-03-08 23:27:09 -0300

    feat: add toggle component markup
    - Reference: https://flowbite.com/docs/forms/toggle/

--- src/app/components/toggle/toggle.component.ts
+++ src/app/components/toggle/toggle.component.ts
@@ -5,8 +5,5 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './toggle.component.html',
-  styleUrl: './toggle.component.scss'
 })
-export class ToggleComponent {
-
-}
+export class ToggleComponent {}
