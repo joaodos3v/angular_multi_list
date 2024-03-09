55401779e23c5b4282498a054a827bdeac28c9b7
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-03-09 20:27:02 -0300

    feat: add Toggle component features to change external API provider

--- src/app/components/toggle/toggle.component.ts
+++ src/app/components/toggle/toggle.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import { SOURCE_PROVIDER } from '../../providers/services/source-service.provider';
 
 @Component({
   selector: 'app-toggle',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './toggle.component.html',
 })
-export class ToggleComponent {}
+export class ToggleComponent implements OnInit {
+  sourceService = inject(SOURCE_PROVIDER);
+
+  toggleValue = false;
+
+  ngOnInit(): void {
+    this.getUsePokeAPIValue();
+  }
+
+  toggleCheckbox() {
+    this.sourceService.toggleUsePokeAPI(this.toggleValue);
+  }
+
+  getUsePokeAPIValue() {
+    this.sourceService.usePokeAPI.subscribe((shouldUse) => {
+      this.toggleValue = shouldUse;
+    });
+  }
+}
