4371366034a34d0aade7fc6d3ef7560fd56aced0
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-03-08 23:38:36 -0300

    feat: create filter-list component

--- src/app/components/controls/controls.component.ts
+++ src/app/components/controls/controls.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FilterListComponent } from '../filter-list/filter-list.component';
 
 @Component({
   selector: 'app-controls',
   standalone: true,
-  imports: [],
+  imports: [FilterListComponent],
   templateUrl: './controls.component.html',
 })
 export class ControlsComponent {}
---
+++ src/app/components/filter-list/filter-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-list.component.html',
+})
+export class FilterListComponent {}
